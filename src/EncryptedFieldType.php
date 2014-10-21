803228a72b974706b3cbc08ba6d3c816a72054d9
Ryan Thompson <service@aiwebsystems.com>
2014-10-21 08:39:06 -0500

    Fixing

--- src/EncryptedFieldType.php
+++ src/EncryptedFieldType.php
@@ -1,8 +1,8 @@
-<?php namespace Anomaly\Streams\FieldType\Encrypted;
+<?php namespace Anomaly\Streams\Addon\FieldType\Encrypted;
 
-use Streams\Core\Addon\FieldTypeAbstract;
+use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
-class EncryptedFieldType extends FieldTypeAbstract
+class EncryptedFieldType extends FieldTypeAddon
 {
     /**
      * The database column type this field type uses.
