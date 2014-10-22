1bbf3e0674ea202f5ff793320f4ddae297aa6766
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:33:22 -0500

    Clean up before final

--- src/EncryptedFieldType.php
+++ src/EncryptedFieldType.php
@@ -4,29 +4,12 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class EncryptedFieldType extends FieldTypeAddon
 {
-    protected $slug = 'encrypted';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
     public $columnType = 'text';
 
-    /**
-     * Available field type settings.
-     *
-     * @var array
-     */
     public $settings = array(
         'hide_typing',
     );
 
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
     public function input()
     {
         $value = \Crypt::decrypt($this->value);
@@ -36,11 +19,6 @@ class EncryptedFieldType extends FieldTypeAddon
         return \Form::input($type, $this->inputName(), $value);
     }
 
-    /**
-     * Encrypt the value upon setting.
-     *
-     * @return mixed
-     */
     public function mutate($value)
     {
         return \Crypt::encrypt($value);
