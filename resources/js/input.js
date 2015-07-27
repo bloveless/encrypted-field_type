c584ea114d0e9fc5d637f8ef87c75b73c9cddffb
Ryan Thompson <service@aiwebsystems.com>
2015-07-27 08:39:37 -0500

    Fix JS issue with multiple inputs

--- resources/js/input.js
+++ resources/js/input.js
@@ -5,15 +5,15 @@ $(function () {
 
         var wrapper = $(this);
 
-        // Toggle text
-        wrapper.on('click', '[data-toggle="text"]', function () {
+        wrapper.find('[data-toggle="text"]').click(function () {
+
             if (wrapper.find('input').attr('type') == 'password') {
                 wrapper.find('input').attr('type', 'text');
             } else {
                 wrapper.find('input').attr('type', 'password');
             }
+
+            return false;
         });
-        
-        return false;
     });
 });
